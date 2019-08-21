6a5de4b5b26386abca0714f636eab4fca0b2e2da
Daniel <daniel.kurniawan@student.umn.ac.id>
2019-08-21 16:14:46 +0700

    Initial Commit

---
+++ TableViewDanielllll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewDanielllll
+//
+//  Created by Daniel Aditya Kurniawan on 21/08/19.
+//  Copyright © 2019 Daniel Aditya Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
